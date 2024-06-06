69b0311ce12d3ce4efa5ef34dc369f45fa78d25c
Adam Ibouroi Soilihi <adamibouroifils@gmail.com>
2024-06-06 14:27:40 +0200

    insertion d'une condition pour l'affichage du menu

--- header.php
+++ header.php
@@ -1,10 +1,17 @@
 <nav>
                 
-                    <img src="#" alt="Logo de mon Blog" >
+    <img src="#" alt="Logo de mon Blog" >
                 
-                <div>
-                    <a href="index.php" >Accueil</a>
-                    <a href="articles.php">Les articles</a>
-                    <a href="contact.php" >Contact</a>
-                </div>
-            </nav>
+    <div>
+        <a href="index.php" >Accueil</a>
+        <a href="articles.php">Les articles</a>
+        <a href="contact.php" >Contact</a>
+      
+        <?php if(isset($_SESSION['loggedUser'])):?>
+            <a href="#" >Se deconnecter</a>
+            <a href="Create_article.php" >Créer un article</a>
+        <?php else:?>
+            <a href="login.php" >Se connecter</a>
+        <?php endif;?>
+    </div>
+</nav>
